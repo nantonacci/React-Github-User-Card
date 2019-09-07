f36a91fc29b808c146e683bcd4737d33db6f720f
Nicole Antonacci <nicole.antonacci@gmail.com>
2019-09-07 11:19:03 -0700

    Cleaned up user card and added styles to follower card.

--- github-user-card/src/components/FollowerCard.js
+++ github-user-card/src/components/FollowerCard.js
@@ -1,20 +1,25 @@
 import React from 'react';
-
+import { Card, Icon } from 'semantic-ui-react';
 
 function FollowerCard(props) {
-
+    
         
     return(
         <div>
-            {props.follower.map(follower => (
-                <div key={follower.id}>
-                <h2>{follower.login}</h2>
-                <img src={follower.avatar_url} alt={follower.login} />
-                <p><a href={follower.url}>View Profile</a></p>
-                </div>
-            ))}
-            
+        {props.follower.map(follower => (
+             <Card
+                key={follower.id}
+                image={follower.avatar_url}
+                alt={follower.login}
+                header={follower.login}
+                meta={<a href={follower.html_url}>Profile</a>}
+                
+              
+                />
+        ))}
         </div>
+        
+               
     )
 }
 
--- github-user-card/src/components/UserCard.js
+++ github-user-card/src/components/UserCard.js
@@ -3,18 +3,29 @@ import { Card, Icon } from 'semantic-ui-react';
 
 function UserCard(props) {
     const extra = (
-        <a>
-          <Icon name='user' />
-          {props.user.followers} followers
+        <div>
+        <a href={props.user.email}>
+            <Icon name='mail' />
         </a>
+
+        <a href={props.user.blog}>
+          <Icon name='twitter' />
+        </a>
+        
+        <a href={props.user.html_url}>
+            <Icon name='github' />
+        </a>
+        
+        </div>
       )
             
     return(
         <Card
         image={props.user.avatar_url}
+        alt={props.user.name}
         header={props.user.name}
         meta={props.user.location}
-        description={<a href={props.user.url}>Check out my profile</a>}
+        description={props.user.bio}
         extra={extra}
         />
         
